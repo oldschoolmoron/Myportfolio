2aad8a28b797d854edc9b1e5c9a743a4ccff4a59
Ananya Pathak <anyalko043@gmail.com>
2025-03-25 15:48:24 +0530

    Fix: Adjusted alignment in the project section

--- app/Components/Work.jsx
+++ app/Components/Work.jsx
@@ -26,7 +26,7 @@ const Work = () => (
         </div>
       ))}
     </div>
-    <a href='' className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full y-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>
+    <a href='https://github.com/oldskoolmoron?tab=repositories' target='_blank'  className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500'>
       Show more <Image src={assets.right_arrow_bold} alt='Right-arrow' className='w-4' />
     </a>
   </div>
